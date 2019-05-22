7787d6edcd93974dd6b91470c0e991592bb23b91
maz_leo <xtleon.xl@gmail.com>
2019-05-23 00:31:25 +0600

    sqllite-creat-insert
    create table upgrade adn insert method implements

--- Database/app/src/main/java/com/leo/classseven/myapplication/MyDatabase.java
+++ Database/app/src/main/java/com/leo/classseven/myapplication/MyDatabase.java
@@ -1,8 +1,11 @@
 package com.leo.classseven.myapplication;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 
@@ -15,27 +18,69 @@ public class MyDatabase extends SQLiteOpenHelper {
     public static final String COL_3 = "email";
     public static final String COL_4 = "phone";
     public static final String ID_COL = "user_id";
+    private static final String DROP_TABLE="DROP TABLE IF EXISTS "+TABLE_NAME;
     public static final  int DATABASE_VERSION = 1 ;
-   // public Context mContext ;
+    public Context context ;
 
 
     public MyDatabase(@Nullable Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
-        // this.mContext = context;
+         this.context = context;
     }
 
 
     @Override
     public void onCreate(SQLiteDatabase db) {
         // to create  database table.
-        String create_table ="CREATE TABLE "+TABLE_NAME+"("+ID_COL+"INTEGER PRIMARY KEY AUTOINCREMENT,"+
-                COL_1+"VARCHAR(255),"+COL_2+"VARCHAR(255) ";
+       try{
+           String create_table ="CREATE TABLE "+TABLE_NAME+"("+ID_COL+"INTEGER PRIMARY KEY AUTOINCREMENT,"+
+                   COL_1+" VARCHAR(255),"+COL_2+" VARCHAR(255), "+COL_3+" VARCHAR(255), "+COL_4+" VARCHAR(255));";
+
+           db.execSQL(create_table);
+       }catch (Exception e )
+       {
+           Toast.makeText(context, "Expception"+e, Toast.LENGTH_SHORT).show();
+       }
 
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         //to update database..
+        try{
+            db.execSQL(DROP_TABLE);
+        }catch (Exception e)
+        {
+            Toast.makeText(context,"Exception is: "+e,Toast.LENGTH_LONG).show();
+        }
 
+    }
+
+    public boolean insertData(String username, String pass, String email, String phone ){
+
+        SQLiteDatabase sDB = this.getWritableDatabase();
+
+        ContentValues conValue = new ContentValues();
+
+        conValue.put(COL_1, username);
+        conValue.put(COL_2, pass);
+        conValue.put(COL_3, email);
+        conValue.put(COL_4, phone);
+
+        long res;
+
+        res = sDB.insert(TABLE_NAME, null,conValue);
+
+        if (res == -1){
+            return false;
+        }else{
+            return true;
+        }
+    }
+    public Cursor displayAllData()
+    {
+        SQLiteDatabase sqLiteDatabase=this.getReadableDatabase();
+        Cursor cursor=sqLiteDatabase.rawQuery("SELECT * FROM "+TABLE_NAME,null);
+        return cursor;
     }
 }
--- Database/app/src/main/java/com/leo/classseven/myapplication/RegisterActivity.java
+++ Database/app/src/main/java/com/leo/classseven/myapplication/RegisterActivity.java
@@ -5,12 +5,14 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class RegisterActivity extends AppCompatActivity {
 
     EditText usernameET, passET, emailET, phoneET;
     Button regButton;
-    MyDatabase db =new MyDatabase(getApplicationContext());
+    MyDatabase saveIntoDatabase;
+
 
 
 
@@ -18,6 +20,7 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        saveIntoDatabase = new MyDatabase(getApplicationContext());
 
         usernameET = findViewById(R.id.username_et);
         passET = findViewById(R.id.pass_et);
@@ -46,6 +49,12 @@ public class RegisterActivity extends AppCompatActivity {
                  //check if any body empty or not
 
         //now store information into database..
+        if(saveIntoDatabase.insertData(userName,pass,email,phone)){
+            Toast.makeText(this, "Data insert successfull", Toast.LENGTH_SHORT).show();
+        }else{
+            Toast.makeText(this, "Data insert Failed!!", Toast.LENGTH_SHORT).show();
+        }
+
 
 
 
